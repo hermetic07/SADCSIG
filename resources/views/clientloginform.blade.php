34d66b7523e6dc060f237eff8ca781df99757145
Liess <ejmaskarino05@gmail.com>
2017-07-18 03:15:35 +0800

    Updated Client and security guard login form

--- resources/views/clientloginform.blade.php
+++ resources/views/clientloginform.blade.php
@@ -14,6 +14,9 @@
 <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- animation CSS -->
 <link href="css/animate.css" rel="stylesheet">
+<!-- Menu CSS -->
+<link href="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+
 <!-- Custom CSS -->
 <link href="css/style.css" rel="stylesheet">
 <!-- color CSS -->
@@ -53,53 +56,118 @@
 
   <div class="login-box3 login-sidebar">
     <div class="white-box">
-      <form class="form-horizontal form-material" id="loginform" action="{{url('/CLogin')}}" method="post">
-  <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <a href="javascript:void(0)" class="text-center db"><img src="plugins/images/users/logoicon2.png" alt="Home" /><br/><img src="plugins/images/users/logotext2.png"  alt="Home" /></a>
-		  </br>	  </br>	  	</br>
-        <div class="form-group m-t-40">
-          <div class="col-xs-12">
-            <input class="form-control" type="text" name="client_username" required="" placeholder="Username">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-xs-12">
-            <input class="form-control" type="password" required="" name="client_password" placeholder="Password">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-md-12">
-
-            <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
-        </div>
-        <div class="form-group text-center m-t-20">
-			</br> </br> </br> </br>
-          <div class="col-xs-12">
-            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit name="submit" value="Log IN"">Log In</button>
-          </div>
-        </div>
 
 
-      </form>
-      <form class="form-horizontal" id="recoverform" action="login2.html">
-        <div class="form-group ">
-          <div class="col-xs-12">
-            <h3>Recover Password</h3>
-            <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
-          </div>
-        </div>
-        <div class="form-group ">
-          <div class="col-xs-12">
-            <input class="form-control" type="text" required="" placeholder="Email">
-          </div>
-        </div>
-        <div class="form-group text-center m-t-20">
-          <div class="col-xs-12">
-            <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
-		  	<button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" onClick="history.go(0)">Cancel</button>
-          </div>
-        </div>
-      </form>
+
+        <a href="javascript:void(0)" class="text-center db"><img src="plugins/images/users/logoicon2.png"width="90px"  alt="Home" /><br/><img src="plugins/images/users/logotext2.png" width="200px" alt="Home" /></a>
+		  </br>  </br> </br>
+      <div class="sttabs tabs-style-flip">
+        <nav>
+          <ul>
+            <li><a href="#section-flip-4" class="sticon fa fa-user"><span>I am a client  </span></a></li>
+                  <li><a href="#section-flip-5" class="sticon ti-shield"><span>I am a guard</span></a></li>
+          </ul>
+        </nav>
+        <div class="content-wrap">
+          <section id="section-flip-1">
+            <form class="form-horizontal form-material animated slideInLeft    " id="loginform"  action="{{url('/CLogin')}}" method="post">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              <div class="form-group m-t-40">
+                <div class="col-xs-12">
+                  <input class="form-control" type="text" name="client_username" required="" placeholder="Email address">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-xs-12">
+                  <input class="form-control" type="password" required="" name="client_password" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-md-12">
+
+                  <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
+              </div>
+              <div class="form-group text-center m-t-20">
+            </br> </br>
+                <div class="col-xs-12">
+                  <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit name="submit" value="Log IN"">Log In</button>
+                </div>
+              </div>
+
+
+            </form>
+            <form class="form-horizontal" id="recoverform" action="login2.html">
+              <div class="form-group ">
+                <div class="col-xs-12">
+                  <h3>Recover Password</h3>
+                  <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
+                </div>
+              </div>
+              <div class="form-group ">
+                <div class="col-xs-12">
+                  <input class="form-control" type="text" required="" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group text-center m-t-20">
+                <div class="col-xs-12">
+                  <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
+              <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" onClick="history.go(0)">Cancel</button>
+                </div>
+              </div>
+            </form>
+          </section>
+          <section id="section-flip-2">
+            <form class="form-horizontal form-material animated slideInRight " >
+
+              <div class="form-group m-t-40">
+                <div class="col-xs-12">
+                  <input class="form-control" type="text"  required="" placeholder="Email address">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-xs-12">
+                  <input class="form-control" type="password" required="" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-md-12">
+
+                  <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
+              </div>
+              <div class="form-group text-center m-t-20">
+            </br> </br>
+                <div class="col-xs-12">
+                  <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" >Log In</button>
+                </div>
+              </div>
+
+
+            </form>
+            <form class="form-horizontal" id="recoverform" action="login2.html">
+              <div class="form-group ">
+                <div class="col-xs-12">
+                  <h3>Recover Password</h3>
+                  <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
+                </div>
+              </div>
+              <div class="form-group ">
+                <div class="col-xs-12">
+                  <input class="form-control" type="text" required="" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group text-center m-t-20">
+                <div class="col-xs-12">
+                  <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
+              <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" onClick="history.go(0)">Cancel</button>
+                </div>
+              </div>
+            </form>
+
+          </section>
+
+        </div><!-- /content -->
+      </div><!-- /tabs -->
+
     </div>
   </div>
 <!-- jQuery -->
@@ -117,5 +185,16 @@
 <script src="js/custom.min.js"></script>
 <!--Style Switcher -->
 <script src="plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+<script src="js/cbpFWTabs.js"></script>
+<script type="text/javascript">
+      (function() {
+
+                [].slice.call( document.querySelectorAll( '.sttabs' ) ).forEach( function( el ) {
+                    new CBPFWTabs( el );
+                });
+
+            })();
+</script>
+
 </body>
 </html>
