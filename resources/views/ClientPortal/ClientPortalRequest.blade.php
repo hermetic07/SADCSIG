96086ba4ddca9edb36af3dfb801866f22baee66f
hermetic07 <evndr.ls@gmail.com>
2017-08-23 23:16:47 +0800

    [fix] quotaion request modal title

--- resources/views/ClientPortal/ClientPortalRequest.blade.php
+++ resources/views/ClientPortal/ClientPortalRequest.blade.php
@@ -562,7 +562,7 @@
               <div class="modal-content">
                 <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                  <h4 class="modal-title" id="myLargeModalLabel"><center><strong>Leave request</strong></center></h4>
+                  <h4 class="modal-title" id="myLargeModalLabel"><center><strong>Quotation</strong></center></h4>
                 </div>
                 <div class="modal-body">
             <form data-toggle="validator" method="POST" action="{{URL::to('/Client-Qoute')}}">
