2aec76ad42ef1fbd2d5fe111274323483a89db93
earl04 <geraldezearldixon04@gmail.com>
2017-10-06 23:56:44 +0800

    revised dispo report

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -7,11 +7,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Charts;
 use App\Clients;
+use App\ClientRegistration;
 use App\Area;
 use App\Province;
 use App\Establishments;
 use PDF;
 use App\EmployeeEducation;
+use App\EstabGuards;
+use App\Contracts;
 
 class ReportsController extends Controller
 {
@@ -77,7 +80,9 @@ class ReportsController extends Controller
                     ->where('tblestabguards.isReplaced','=','0')
                     ->join('establishments','establishments.id','=','tblestabguards.strEstablishmentID')
                      ->join('areas','areas.id','=','establishments.areas_id')
-                        ->join('provinces','provinces.id','=','areas.provinces_id')
+                    ->join('provinces','provinces.id','=','areas.provinces_id')
+                    ->join('client_registrations','client_registrations.contract_id','=','tblestabguards.contractID')
+                    ->join('clients','clients.id','=','client_registrations.client_id')
                     ->select('employees.id as empID',
                             'employees.first_name',
                             'employees.middle_name',
@@ -87,18 +92,40 @@ class ReportsController extends Controller
                             'security_license.date_expired',
                             'establishments.address as address',
                             'areas.name as area',
-                            'provinces.name as province'
+                            'provinces.name as province',
+                            'clients.id as client_id'
                             )
                     ->whereBetween('tblestabguards.created_at', [$request->startFrom, $request->endTo])
                     ->get();
+                    //return response($dispositions->count());
+        $clients = Clients::all();
+        $contracts = Contracts::all();
+        $client_registrations = ClientRegistration::all();
+        $establishments = DB::table('establishments')
+                            
+                        ->join('areas','areas.id','=','establishments.areas_id')
+                        ->join('provinces','provinces.id','=','areas.provinces_id')
+                        ->select('establishments.name as estabname',
+                            'establishments.contract_id',
+                            'establishments.id',
+                            'establishments.address',
+                            'areas.name as area',
+                            'provinces.name as province')
+                        ->get();
         //return $dispositions->toArray();
         $employee_educations = EmployeeEducation::all();
+        $estabGuards = EstabGuards::where('isReplaced','=','0')->get();
         $dispostionReportPDF = PDF::loadView('AdminPortal.Reports_PDF.disposition_report',
                     [
                       'start' => $request->startFrom,
                       'end' => $request->endTo,
                       'dispositions' => $dispositions,
-                      'employee_educations' => $employee_educations
+                      'employee_educations' => $employee_educations,
+                      'clients' => $clients,
+                      'client_registrations' => $client_registrations,
+                      'establishments' => $establishments,
+                      'estabGuards' => $estabGuards,
+                      'contracts' => $contracts
                     ]);
       return $dispostionReportPDF->stream('Disposition Report.pdf');
     }
